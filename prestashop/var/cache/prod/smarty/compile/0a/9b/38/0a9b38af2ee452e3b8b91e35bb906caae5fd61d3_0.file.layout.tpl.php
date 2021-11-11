7c995cf44dc6ffb2905fe6dfa1165aa0f417d5da
robeat1 <dzienisz.robert@gmail.com>
2021-11-11 19:47:43 +0100

    init

---
+++ prestashop/var/cache/prod/smarty/compile/0a/9b/38/0a9b38af2ee452e3b8b91e35bb906caae5fd61d3_0.file.layout.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-11-11 19:27:27
+  from '/var/www/html/admin6/themes/default/template/controllers/login/layout.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_618d608f9b1743_34571140',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '0a9b38af2ee452e3b8b91e35bb906caae5fd61d3' => 
+    array (
+      0 => '/var/www/html/admin6/themes/default/template/controllers/login/layout.tpl',
+      1 => 1633363913,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_618d608f9b1743_34571140 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+
+<?php }
+}
---
+++ prestashop/var/cache/prod/smarty/compile/classic/8d/6e/9e/8d6e9eb0f4bdd313532097a19e68b4922e0b841c_0.file.popup.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-11-11 19:38:13
+  from '/var/www/html/modules/welcome/views/templates/popup.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_618d63158d9954_11701701',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8d6e9eb0f4bdd313532097a19e68b4922e0b841c' => 
+    array (
+      0 => '/var/www/html/modules/welcome/views/templates/popup.tpl',
+      1 => 1624549278,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_618d63158d9954_11701701 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="onboarding-popup bootstrap">
+  <div class="content"></div>
+</div>
+<?php }
+}
